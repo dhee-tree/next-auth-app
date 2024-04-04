cbe3320390144618df44afb8f9051b04145191a3
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2024-04-04 20:18:24 +0100

    Initial homepage

--- app/(protected)/member/page.jsx
+++ app/(protected)/member/page.jsx
@@ -1,15 +1,19 @@
 import { auth } from "@/auth"
 
 const member = async () => {
-
   const session = await auth()
-
+  
   return (
     <div>
       <h1>Member Server Session</h1>
+      <p>Name: {session.user.name}</p>
+      <p>Email: {session.user.email}</p>
+      <p>Role: {session.user.role}</p>
       {JSON.stringify(session)}
     </div>
   )
 }
+
+  
 
 export default member
