76141cd0606cc46edc6dc59e9461b586913fb068
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2024-04-04 20:17:36 +0100

    Added custom styles, and initial home page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,20 @@
 const Home = () => {
   return (
-    <div className="pt-2">
-      <h1>Home</h1>
-      <p>
-        Welcome to the Home page. This page is accessible to all users.
-      </p>
-    </div>
+    <main className="bg-warning">
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          <div className="col text-center">
+            <h1 className="pt-5">Next Auth</h1>
+            <p>Authentication for Next.js</p>
+            <button className="btn btn-primary">
+              Sign in
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
   )
+
 }
 
 export default Home
